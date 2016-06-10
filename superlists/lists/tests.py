47ea03ed7675bee232fe72b4239258eabb1fd0f9
lenarother <rother.magdalena@gmail.com>
2016-06-10 12:51:44 +0200

    Basic view returns html

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from django.test import TestCase
 
 from lists.views import home_page
@@ -6,6 +7,13 @@ from lists.views import home_page
 
 class HomePageTests(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		find = resolve('/')
-		self.assertEqual(find.func, home_page)
+    def test_root_url_resolves_to_home_page_view(self):
+        find = resolve('/')
+        self.assertEqual(find.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
