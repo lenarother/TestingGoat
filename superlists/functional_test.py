05aa691e113fb601b30c3d1dc1cd00665387208a
lenarother <rother.magdalena@gmail.com>
2016-07-02 21:06:22 +0200

    Add simple form

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -21,6 +21,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_table_list(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -43,23 +48,25 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists:
         # "1: Buy peacock feathers" as an item in a to-do lists
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        expected_text = '1: Buy peacock feathers'
-        error_message = "New to-do item did not appear in table"
-        self.assertTrue(any(row.text == expected_text for row in rows),
-                        error_message)
+        self.check_for_row_in_table_list('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her lists
+        self.check_for_row_in_table_list('1: Buy peacock feathers')
+        self.check_for_row_in_table_list(
+            '2: Use peackok feathers to make a fly'
+        )
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explenatory text to that effect.
+        self.fail('Finish the test!')
 
         # She visits the URL - her to-do list is still there.
 
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -15,5 +15,20 @@ class HomePageTests(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        # passing request to render_to_string makes
+        # this function work with crsf_token
+        expected_html = render_to_string(
+            'home.html',
+            {'item_text': 'A new list item'},
+            request=request)
+
         self.assertEqual(expected_html, response.content.decode())
+
+    def test_home_page_can_save_a_POST_requet(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
