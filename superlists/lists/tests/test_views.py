4514a369ef44001b139e040f4cbc4e79f260f1a8
lenarother <rother.magdalena@gmail.com>
2016-07-23 07:36:04 +0200

    Remove obsolete view

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -55,34 +55,6 @@ class NewListTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-class NewItemTest(TestCase):
-
-    def test_can_save_a_POST_requet_to_an_existing_list(self):
-        List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            '/lists/{}/add_item'.format(correct_list.id),
-            data={'text': 'A new item for an existing list'}
-        )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new item for an existing list')
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/{}/add_item'.format(correct_list.id),
-            data={'text': 'A new item for an existing list'}
-        )
-
-        self.assertRedirects(response, '/lists/{}/'.format(correct_list.id))
-
-
 class ListViewTest(TestCase):
 
     def post_invalid_input(self):
